91326a866d3d4fcb5ce4b5406127de591d5f7094
Giga Razki Arianda <75737741+gigarazkiarianda@users.noreply.github.com>
2025-01-25 21:49:15 +0700

    learn django

---
+++ myapp/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Item
+from .forms import ItemForm
+
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'myapp/list.html', {'items': items})
+
+def item_create(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('item_list')
+    else:
+        form = ItemForm()
+    return render(request, 'myapp/create.html', {'form': form})
+
+def item_update(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('item_list')
+    else:
+        form = ItemForm(instance=item)
+    return render(request, 'myapp/update.html', {'form': form})
+
+def item_delete(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('item_list')
+    return render(request, 'myapp/delete.html', {'item': item})
